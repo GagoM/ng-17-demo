76f0d7db8beb3105f11e016225ff433d4a55ff54
Gilad Mautner <giladregba@gmail.com>
2024-04-15 11:56:26 +0300

    added import for header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ChildrenOutletContexts, RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   animations: []
@@ -14,9 +15,4 @@ export class AppComponent {
 
   constructor(private contexts: ChildrenOutletContexts) {}
 
-  getRouteAnimationData() {
-    return this.contexts.getContext('primary')?.route?.snapshot?.data?.[
-      'animation'
-    ];
-  }
 }
